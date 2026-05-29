f3fa88d84dccdaff2ce5343701ef10972a91c1bd
Al-raafi C. Indanan <alraafiindanan50@gmail.com>
2026-05-29 23:33:08 +0800

    git commit -m "Add exarcise_7.py with the solution to the exercise"

---
+++ exarcise_7.py
@@ -0,0 +1,108 @@
+from typing import Annotated
+import wikipedia
+from langchain_core.tools import tool
+import pandas as pd
+import os
+
+@tool
+def wikipedia_tool(
+    query: Annotated[str, "The Wikipedia search to execute to find key summary information."],
+):
+    """Use this to search Wikipedia for factual information."""
+    try:
+        # Step 1: Search using query
+        results = wikipedia.search(query)
+        
+        if not results:
+            return "No results found on Wikipedia."
+        
+        # Step 2: Retrieve page title
+        title = results[0]
+
+        # Step 3: Fetch summary
+        summary = wikipedia.summary(title, sentences=8, auto_suggest=False, redirect=True)
+    except BaseException as e:
+        return f"Failed to execute. Error: {repr(e)}"
+    return f"Successfully executed:\nWikipedia summary: {summary}"
+
+@tool
+def stock_data_tool(
+    company_ticker: Annotated[str, "The ticker symbol of the company to retrieve their stock performance data."], 
+    num_days: Annotated[int, "The number of business days of stock data required to respond to the user query."]
+) -> str:
+    """
+    Use this to look-up stock performance data for companies to retrieve a table from a CSV. You may need to convert company names into ticker symbols to call this function, e.g, Apple Inc. -> AAPL, and you may need to convert weeks, months, and years, into days.
+    """
+    
+    # Load the CSV for the company requested
+    file_path = f"data/{company_ticker}.csv"
+
+    if os.path.exists(file_path) is False:
+        return f"Sorry, but data for company {company_ticker} is not available. Please try Apple, Amazon, Meta, Microsoft, Netflix, or Tesla."
+    
+    stock_df = pd.read_csv(file_path, index_col='Date', parse_dates=True)
+
+    # Ensure the index is in date format
+    stock_df.index = stock_df.index.date
+    
+    # Maximum num_days supported by the dataset
+    max_num_days = (stock_df.index.max() - stock_df.index.min()).days
+    
+    if num_days > max_num_days:
+        return "Sorry, but this time period exceeds the data available. Please reduce it to continue."
+    
+    # Get the most recent date in the DataFrame
+    final_date = stock_df.index.max()
+
+    # Filter the DataFrame to get the last num_days of stock data
+    filtered_df = stock_df[stock_df.index > (final_date - pd.Timedelta(days=num_days))]
+
+    return f"Successfully executed the stock performance data retrieval tool to retrieve the last *{num_days} days* of data for company **{company_ticker}**:\n\n{filtered_df.to_markdown()}"
+
+from langchain_experimental.utilities import PythonREPL
+
+repl = PythonREPL()
+
+@tool
+def python_repl_tool(
+    code: Annotated[str, "The python code to execute to generate your chart."],
+):
+    """Use this to execute python code. If you want to see the output of a value,
+    you should print it out with `print(...)`. This is visible to the user. The chart should be displayed using `plt.show()`."""
+    try:
+        result = repl.run(code)
+    except BaseException as e:
+        return f"Failed to execute. Error: {repr(e)}"
+    return f"Successfully executed the Python REPL tool.\n\nPython code executed:\n\`\`\`python\n{code}\n\`\`\`\n\nCode output:\n\`\`\`\n{result}\`\`\`"
+
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+
+# Add three tools to the list: wikipedia_tool, stock_data_tool, and python_repl_tool
+tools = [wikipedia_tool, stock_data_tool, python_repl_tool]
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+# Create an agent using the create_react_agent function
+prompt = """
+You are an assistant for research and analysis of Fortune 500 companies. You have access to three tools:
+- A Wikipedia tool for retrieving factual summary information about companies
+- A stock performance data tool for retrieving stock price information from local CSV files
+- A Python tool for executing Python code, which is to be used for creating stock performance visualizations
+"""
+
+# Create an agent using the create_react_agent function
+agent = create_react_agent(
+    llm,
+    tools=tools,
+    name="finance_assistant",
+    prompt=prompt
+    
+)
+
+from course_helper_functions import pretty_print_messages
+
+for chunk in agent.stream(
+    {"messages": [{"role": "user", "content": "Tell me Tesla's current CEO, their latest stock price, and generate a plot of the closing price with the most up-to-date data you have available."}]}
+):
+    pretty_print_messages(chunk)
